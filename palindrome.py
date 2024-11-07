65452b556bf1c7c54e2c1303bf4b4449c6bfd7d5
Likitha-gifty <likithadurgalla@msitprogram.net>
2024-11-07 16:47:17 +0530

    palidrome

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+def palindrome(a):
+    print(m)
+    print(m[::-1])
+    if m == m[::-1]:
+        return "is palindrome!"
+    else:
+        return "not a palindrome"
+
+m = "mam"
+print(palindrome(m))
